9b0e3e8345c8ff83f651983452f959ee2cf956de
a186r <hi.aewalle@gmail.com>
2018-06-29 16:46:49 +0800

    update

--- ls.rs
+++ ls.rs
@@ -10,29 +10,76 @@
 // 生命周期是可以写多个的，用,分隔
 // 生命周期与泛型都写在<>里，先生命周期后泛型，用,分隔
 
-#[derive(Copy, Clone)]
-struct A {
-    a: i32,
-}
+// #[derive(Copy, Clone)]
+// struct A {
+//     a: i32,
+// }
 
-impl A {
-    pub fn show(&self) {
-        println!("{}", self.a);
-    }
+// impl A {
+//     pub fn show(&self) {
+//         println!("{}", self.a);
+//     }
 
-    pub fn add_two(&mut self) {
-        self.add_one();
-        self.add_one();
-        self.show();
-    }
+//     pub fn add_two(&mut self) {
+//         self.add_one();
+//         self.add_one();
+//         self.show();
+//     }
 
-    pub fn add_one(&mut self) {
-        self.a += 1;
-    }
+//     pub fn add_one(&mut self) {
+//         self.a += 1;
+//     }
+// }
+
+// fn main() {
+//     let mut ast = A { a: 12i32 };
+//     ast.show();
+//     ast.add_two();
+// }
+
+// enum SpecialPoint {
+//     Point(i32, i32),
+//     Special(String),
+// }
+
+// enum SpecialPoint {
+//     Point { x: i32, y: i32 },
+//     Special(String),
+// }
+
+// 枚举访问成员需要用到模式匹配
+// enum SpecialPoint {
+//     Point(i32, i32),
+//     Special(String),
+// }
+// fn main() {
+//     let sp = SpecialPoint::Point(0, 0);
+//     match sp {
+//         SpecialPoint::Point(x, y) => {
+//             println!("I'am SpecialPoint(x={}, y={})", x, y);
+//         }
+//         SpecialPoint::Special(why) => {
+//             println!("I'am Special because I am {}", why);
+//         }
+//     }
+// }
+
+// struct Point {
+//     x: i32,
+//     y: i32,
+// }
+
+// let point = Point{x:1,y:2};
+
+// let Point{x:x,y:y} = point;
+
+// let Point{x,y} = point;
+
+fn use_str(s: &str) {
+    println!("i am {}", s);
 }
 
 fn main() {
-    let mut ast = A { a: 12i32 };
-    ast.show();
-    ast.add_two();
+    let s = "Hello".to_string();
+    use_str(&*s);
 }
