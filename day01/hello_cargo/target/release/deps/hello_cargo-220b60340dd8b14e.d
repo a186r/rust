f511e80b90d43a03782f97a56834ba2e430936a9
a186 <hi.aewalle@gmail.com>
2018-09-04 15:33:48 +0800

    cargo build & cargo run

---
+++ day01/hello_cargo/target/release/deps/hello_cargo-220b60340dd8b14e.d
@@ -0,0 +1,5 @@
+/home/a186r/dev/rust/rust/day01/hello_cargo/target/release/deps/hello_cargo-220b60340dd8b14e: src/main.rs
+
+/home/a186r/dev/rust/rust/day01/hello_cargo/target/release/deps/hello_cargo-220b60340dd8b14e.d: src/main.rs
+
+src/main.rs:
