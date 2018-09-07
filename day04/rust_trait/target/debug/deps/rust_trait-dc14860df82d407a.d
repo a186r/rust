c730cbd0d28f2ee209f7ad369fbf161035fadc0d
a186 <hi.aewalle@gmail.com>
2018-09-07 17:34:21 +0800

    trait没看太懂，先大概过一下，回头再看

---
+++ day04/rust_trait/target/debug/deps/rust_trait-dc14860df82d407a.d
@@ -0,0 +1,5 @@
+/home/a186r/dev/rust/rust/day04/rust_trait/target/debug/deps/librust_trait-dc14860df82d407a.rlib: src/lib.rs
+
+/home/a186r/dev/rust/rust/day04/rust_trait/target/debug/deps/rust_trait-dc14860df82d407a.d: src/lib.rs
+
+src/lib.rs:
---
+++ day04/rust_trait/target/debug/librust_trait.d
@@ -0,0 +1,1 @@
+/home/a186r/dev/rust/rust/day04/rust_trait/target/debug/librust_trait.rlib: /home/a186r/dev/rust/rust/day04/rust_trait/src/lib.rs
