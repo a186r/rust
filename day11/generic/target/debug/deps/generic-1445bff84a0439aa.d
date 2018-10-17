c20989deb8d10e6570fcc48b0b159363bf4d8918
a186r <hi.aewalle@gmail.com>
2018-10-17 18:32:45 +0800

    修改gitignore文件

---
+++ day11/generic/target/debug/deps/generic-1445bff84a0439aa.d
@@ -0,0 +1,5 @@
+/home/a186r/dev/rust/rust/day11/generic/target/debug/deps/generic-1445bff84a0439aa: src/main.rs
+
+/home/a186r/dev/rust/rust/day11/generic/target/debug/deps/generic-1445bff84a0439aa.d: src/main.rs
+
+src/main.rs:
